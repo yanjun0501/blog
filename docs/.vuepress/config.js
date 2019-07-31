9d44990d99a80785deae278fbaa9f1439c5c55f1
yanjun0501 <i@yanjun-forever.cn>
2019-07-31 14:46:23 +0800

    feat: create index

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -49,32 +49,38 @@ module.exports = {
           { text: '后端(No)', link: '/technology/be/' },
         ],
       },
-      { text: 'About', link: '/about/' },     
+      { text: 'About', link: '/about' },     
     ],
-    // sidebar: 'auto'
-    sidebar:{
-      // docs文件夹下面的accumulate文件夹 文档中md文件 书写的位置(命名随意)
-      '/accumulate/': [
-          '/accumulate/', // accumulate文件夹的README.md 不是下拉框形式
-          {
-            title: '标题1',
-            children: [
-              '/accumulate/test', // 以docs为根目录来查找文件 
-              // 上面地址查找的是：docs>accumulate>JS>test.md 文件
-              // 自动加.md 每个子选项的标题 是该md文件中的第一个h1/h2/h3标题
-            ]
-          }
-        ],
-        // docs文件夹下面的algorithm文件夹 这是第二组侧边栏 跟第一组侧边栏没关系
-        '/algorithm/': [
-          // '/algorithm1233/', 
-          {
-            title: '标题1',
-            children: [
-              '/algorithm/test' 
-            ]
-          }
-        ]
+    // sidebar: 'auto',
+    displayAllHeaders: true,
+    sidebar: {
+      '/life/': [
+        '',     /* /foo/ */
+      ],
+      '/my/music/': [
+        '',
+      ],
+      '/my/dao/': [
+        '',
+      ],
+      '/my/acg/': [
+        '',
+        'test'
+      ],
+      '/technology/fe/': [
+        '',
+      ],
+      '/technology/be/': [
+        '',
+      ],
+      '/technology/node/': [
+        '',
+      ],
+      // fallback
+      '/': [
+        // '',        /* / */
+        'about'    /* /about.html */
+      ]
     }
   }
 };
--- docs/life/README.md
+++ docs/life/README.md
@@ -1,4 +1,4 @@
 ### 前言
 
-#### &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;不知道为什么捡起来了大概两年多没碰过的博客，两年间心态发生了变化，对那些绚丽的主题失去了热情。
-#### &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;所以最终选择了vuepress，简单灵活可配置。恩，就这样，希望可以每天产出一点有用或者有意义东西，然后日积月累拼凑成一篇篇文章。
+#### &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;不知道为什么捡起来了大概两年多没碰过的博客，两年间心态发生了巨大变化，现在也对那些绚丽的主题失去了热情。
+#### &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;所以最终选择了Vuepress，简单灵活可配置。恩，就这样，希望可以每天产出一点有用或者有意义东西，然后日积月累拼凑成一篇篇文章。
